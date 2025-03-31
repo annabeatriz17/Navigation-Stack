39c9fe800d83bd1317082a2405bb646bc800c3ac
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-03-31 20:48:34 -0300

    update StackScreen.js

---
+++ my-app/pages/StackScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {  } from "react-native";
+
+export default function StackScreen( { navigation } ) {
+    return (
+        <View styles={styles.container}>
+            <Button title= "Ir para outra tela" onPress={() => navigation.navigate("Stack 2")} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#f652a0",
+    },
+});
